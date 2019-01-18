5131ab29f8010d0ef8c0300ba17f11eb7ec02be9
Avery Bradley <cbra067@lasierra.edu>
2019-01-17 17:32:45 -0800

    init

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,8 @@
+module.exports = {
+    app.get('/api/friends', (req, res) => {
+        //display JSON of all friends
+    }),
+    app.post('/api/friends', (req, res) => {
+        //handles incoming survey results and compatibility logic
+    })
+}
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,8 @@
+module.exports = {
+    app.get('/survey', (req, res) => {
+        //display survey page
+    }),
+    app.get('/*', (req, res) => {
+        //display home page
+    })
+}
