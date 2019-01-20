e8e8a6363f1c32b66cbbddb81ce8f591330ac932
Avery <cbra067@lasierra.edu>
2019-01-19 17:27:46 -0800

    finished front-end and started back-end

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,8 +1,11 @@
-module.exports = {
-    app.get('/survey', (req, res) => {
-        //display survey page
+const path = require('path');
+
+module.exports = function(app) {
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     }),
-    app.get('/*', (req, res) => {
-        //display home page
+    
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
     })
 }
--- server.js
+++ server.js
@@ -3,3 +3,14 @@ const path = require('path');
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require('./app/routing/htmlRoutes')(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
